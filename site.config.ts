ecd90692fe5a78b8153294834baaa8a8bfd1974f
ErikNordquist <110246131+ErikNordquist@users.noreply.github.com>
2023-01-02 20:23:19 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7167d2cf133a4875b7e23caf545ee402',
+  rootNotionPageId: 'faa8ad2c29cd4910933c94abd18f459c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -53,15 +53,15 @@ export default siteConfig({
   navigationStyle: 'custom',
   navigationLinks: [   
     {
-      title: 'Blog',
-      pageId: '588031c1e65d4dfa9d58e2492c9c1b7e'
+      title: 'Blogg',
+      pageId: '0ccbd0f927a1483da6901ae014ce4f08'
     },    
     {
-      title: 'About',
+      title: 'Templates',
       pageId: 'cf2ae3c4839e4e09a1b4a33cc600c409'
     },
     {
-      title: 'Contact',
+      title: 'Kontakt',
       pageId: '5f7e562fef814299b10c68c52e920c9a'
     }
   ]
