bace18d8f482629d6c1817bb21d3b8d219b6df01
ErikNordquist <110246131+ErikNordquist@users.noreply.github.com>
2023-01-18 06:53:41 +0100

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -73,7 +73,7 @@ export const PageHead: React.FC<
       <script
         dangerouslySetInnerHTML={{
           __html:
-            "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-18VCN2NPFB')"
+            "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-18VCN2NPFB',{ 'anonymize_ip': true })"
         }}
       />
       
